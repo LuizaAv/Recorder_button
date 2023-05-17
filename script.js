0a76c1be2c20cf40e8793144095dbbd5b6d73866
LuizaAv <l.avetisyan7777@gmail.com>
2023-05-17 23:53:53 +0400

    created a button which has  functionality to record voice and log the  text in cosole

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+const recButton = document.getElementById("recBtn");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+let record = false;
+let recognition;
+
+function converter(){
+    try{
+        recognition = new SpeechRecognition()
+        recButton.classList.add("record")
+        recognition.start()
+        recognition.onresult = (event) => {
+            const result = event.results[0][0].transcript;
+            console.log(result)
+          };
+        
+          recognition.onspeechend = () => {
+            converter()
+          }
+
+          recognition.onerror = function(event) {
+            console.error(event.error);
+        };
+    }catch(error) {
+        console.error('Error accessing microphone:', error);
+    }
+}
+
+function stopRecording(){
+    recognition.stop()
+    recButton.classList.remove("record")
+    record = false
+}
+
+recButton.addEventListener("keydown", () => {
+    if(!record){
+        converter()
+        record = true
+    }else{
+        stopRecording()
+    }
+})
